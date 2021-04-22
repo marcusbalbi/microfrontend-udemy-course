5120b44b08badb5b6bb600a750fb20d87dc30bd2
Marcus Balbi <balbimarcus@gmail.com>
2021-04-22 18:01:28 -0300

    test push

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,6 +3,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
+
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
