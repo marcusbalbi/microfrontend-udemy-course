1a7e11d3eadfaaad64e0c90788b7c1aec11e968f
Marcus Balbi <balbimarcus@gmail.com>
2021-04-22 15:28:47 -0300

    prod container

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -8,10 +10,15 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-react", "@babel/preset-env"],
-            plugins: ['@babel/plugin-transform-runtime']
+            plugins: ["@babel/plugin-transform-runtime"],
           },
         },
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
