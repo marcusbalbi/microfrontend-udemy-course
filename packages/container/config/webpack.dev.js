c84223d0d45fe30f4fdbdd87192c96c6f920ca0a
Marcus Balbi <balbimarcus@gmail.com>
2021-04-22 18:14:34 -0300

    push to action

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,7 +2,6 @@ const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json")
-
 const devConfig = {
   mode: "development",
   devServer: {
