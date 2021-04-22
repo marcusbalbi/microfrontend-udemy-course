3fb209e6f1504af3efc030dd1b464e93fe9ff075
Marcus Balbi <balbimarcus@gmail.com>
2021-04-21 21:33:23 -0300

    webpack working

--- products/src/index.js
+++ products/src/index.js
@@ -4,7 +4,7 @@ import faker from "faker";
 let products = ''
 
 
-for(let i = 0; i< 3; i++) {
+for(let i = 0; i< 5; i++) {
   const name = faker.commerce.productName();
   products += `<div>${name}</div>`;
 
