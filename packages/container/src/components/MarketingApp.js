2a15ad6f9b442e21b6527eb4a3e1b436713eea16
Marcus Balbi <balbimarcus@gmail.com>
2021-04-23 15:01:10 -0300

    change browser navigation on container

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,16 +1,21 @@
-import React, { useRef, useEffect } from "react"
+import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
-
+import { useHistory } from "react-router-dom";
 const MarketingApp = () => {
   const ref = useRef(null);
-
+  const history = useHistory();
   useEffect(() => {
-    mount(ref.current)
-  }, [])
+    mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+  }, []);
 
-  return (
-    <div ref={ref} />
-  )
-}
+  return <div ref={ref} />;
+};
 
 export default MarketingApp;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"
+import App from "./App";
+import { createMemoryHistory } from "history";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+
+  history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === "development") {
